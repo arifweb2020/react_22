9edfd015046696061629222f25812cfaa180d909
arifweb2020 <arifhussainb2019@gmail.com>
2022-06-17 18:03:39 +0530

    circle skeleton

--- src/comonents/skeleton/SkeletonLoader.js
+++ src/comonents/skeleton/SkeletonLoader.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
-function SkeletonLoader({ width, height }) {
+function SkeletonLoader({ width, height,count,circle }) {
     return (
         <SkeletonTheme highlightColor="#e8ffd1">
-            <Skeleton style={{ width: width, height: height }} />
+            {
+                circle ? <Skeleton style={{ width: width, height: height ,}} count={count} circle /> 
+                : <Skeleton style={{ width: width, height: height ,}} count={count}  />
+            }
+            
         </SkeletonTheme>
     );
 }
--- src/features/about/About.js
+++ src/features/about/About.js
@@ -3,13 +3,44 @@ import { useSelector } from 'react-redux';
 import SkeletonLoader from '../../comonents/skeleton/SkeletonLoader';
 
 function About(props) {
-    const counter = useSelector((state) => state.counter.value)
+    const counter = useSelector((state) => state.counter.value);
+    const [data, setData] = React.useState([]);
+    const [loading, setLaoding] = React.useState(false)
+
+    React.useEffect(() => {
+        const getData = async () => {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users")
+            const res1 = await res.json()
+            setData(res1)
+            setLaoding(true)
+        }
+        getData()
+    }, [])
 
 
     return (
-        <div style={{maxWidth:"80%",margin:"0 auto"}}>
+        <div style={{ maxWidth: "80%", margin: "0 auto" }}>
             <h1>{counter}</h1>
-            <SkeletonLoader width="90px" height="100px"/>
+           
+            {
+                loading
+                ?
+                (
+                    <>
+                    <SkeletonLoader width="30px" height="30px" circle="circle"/>
+                      <SkeletonLoader width="290px" height="30px" count="10"/>
+                       
+                    </>
+                )
+                :
+                (
+                data.map((ele, i) => {
+                    return <div key={i}>
+                        <h4>{ele.name}  </h4>
+                    </div>
+                })
+                )
+            }
         </div>
     );
 }
