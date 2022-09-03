5d2593c84ad32c5db8b6a0687792bd5c47b73db6
arifweb2020 <arifhussainb2019@gmail.com>
2022-09-03 13:05:01 +0530

    env file

---
+++ src/api/howtomakeenv.js
@@ -0,0 +1,18 @@
+// for .env.production
+
+// PUBLIC_URL=/arif-automation
+// REACT_APP_ADSSO_PATH=/payout-adsso
+// REACT_APP_API_URL=https://arif.com/payout-backend
+// REACT_APP_ROUTER_BASE_NAME=/arif-automation
+// REACT_APP_API_ENCRYPT_KEY=122324324
+// REACT_APP_API_ENCRYPT_IV=23434354356
+
+
+// for .env
+
+// REACT_APP_PUBLIC_URL=https://localhost
+// REACT_APP_ROUTER_BASE_NAME=""
+// REACT_APP_ADSSO_PATH=/arif-adsso
+// REACT_APP_API_URL=http://microservice-arif/api-backend
+// REACT_APP_API_ENCRYPT_KEY=1234344
+// REACT_APP_API_ENCRYPT_IV=3435446
