6cf5a0bf4b87971b5e022ee9da86f8eccb8f755e
arifweb2020 <arifhussainb2019@gmail.com>
2022-09-03 15:44:11 +0530

    env set up

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -1,5 +1,11 @@
 export const endpointURL = process.env.REACT_APP_API_URL;
 
+// export const endpointURL1 =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_URL
+//     : "http://microservice-arif/api-backend";
+
+
 
 export const endpointNames = {
     patient:{
--- src/api/howtomakeenv.js
+++ src/api/howtomakeenv.js
@@ -1,6 +1,7 @@
 // for .env.production
 
 // PUBLIC_URL=/arif-automation
+// ENV NODE_ENV=production
 // REACT_APP_ADSSO_PATH=/payout-adsso
 // REACT_APP_API_URL=https://arif.com/payout-backend
 // REACT_APP_ROUTER_BASE_NAME=/arif-automation
@@ -16,3 +17,27 @@
 // REACT_APP_API_URL=http://microservice-arif/api-backend
 // REACT_APP_API_ENCRYPT_KEY=1234344
 // REACT_APP_API_ENCRYPT_IV=3435446
+
+
+// https://stackoverflow.com/questions/66365158/how-to-setup-the-env-variable-for-the-react-app
+
+// https://www.codingdeft.com/posts/react-environment-variables/
+
+// Constants.js
+// const prod = {
+//     url: {
+//      API_URL: ‘https://myapp.herokuapp.com',
+//      API_URL_USERS: ‘https://myapp.herokuapp.com/users'}
+//    };
+//    const dev = {
+//     url: {
+//      API_URL: ‘http://localhost:3000'
+//     }
+//    };
+//    export const config = process.env.NODE_ENV === ‘development’ ? dev : prod;
+
+
+//    // Component.js
+// import { config } from './Constants'
+// var url = config.url.API_URL
+// var url_users = config.url.API_URL_USERS
